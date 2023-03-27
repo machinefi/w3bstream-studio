f3281c7293fb804cdce0d48010e505795ad2d96d
osdodo <osdodo@qq.com>
2023-03-27 11:41:19 +0800

    update login page

--- src/pages/admin-login.tsx
+++ src/pages/admin-login.tsx
@@ -1,34 +1,42 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
-import { Box, Flex, Image, Text } from '@chakra-ui/react';
-import { Center } from '@chakra-ui/layout';
-import { JSONForm } from '@/components/JSONForm';
 import { useStore } from '../store';
+import { Box, Flex, Icon, Image, Link, Text } from '@chakra-ui/react';
+import { JSONForm } from '@/components/JSONForm';
+import { BsArrowRight } from 'react-icons/bs';
 
 const Login = observer(() => {
   const { w3s } = useStore();
 
   return (
-    <Box w="100vw" h="100vh" minW="1440px" bg="linear-gradient(to right, #EBF2FC, #E3DEFC)">
-      <Image w="200px" pos="fixed" top="70px" left="100px" src="/images/logo.svg" alt="logo" />
-      <Image pos="fixed" top="0px" left="52%" src="/images/polygon_1.svg" alt="" />
-      <Image pos="fixed" bottom="0px" right="0px" src="/images/polygon_3.svg" alt="" />
-      <Center h="100%" p="20px">
+    <Box w="100vw" h="100vh" minW="1440px" bg={`center / cover no-repeat url("../images/bg.png")`}>
+      <Flex w="100%" h="100%" justifyContent="center" alignItems="center">
         <Flex>
           <Box>
-            <Text fontSize="30px" fontWeight={700} color="#0D0D0D">
-              Welcome to W3bstream Studio
-            </Text>
-            <Image mt="120px" w="350px" src="/images/polygon_2.svg" alt="" />
+            <Flex alignItems="center">
+              <Image w="60px" src="/favicon.svg" alt="logo" />
+              <Box ml="20px" fontWeight={700} fontSize="30px">
+                W3bstream studio
+              </Box>
+            </Flex>
+            <Box mt="100px" fontWeight={700} fontSize="24px">
+              Documentation
+            </Box>
+            <Box mt="10px" maxW="500px" fontWeight={700} fontSize="16px" color="#7A7A7A">
+              Get the most out of using Fastly for your site, service, orapplication through a series of articles and tutorials.
+            </Box>
+            <Link isExternal href="#" color="#946FFF" fontSize="14px">
+              Read Full <Icon ml="5px" mb="-2px" as={BsArrowRight} />
+            </Link>
           </Box>
-          <Box zIndex={9} ml="120px" w="580px" p="40px" bg="#fff" borderRadius="8px">
-            <Text mb="30px" fontSize="30px" fontWeight={700} color="#1B1B1B">
+          <Box zIndex={9} ml="130px" w="600px" p="80px 50px" bg="linear-gradient(180deg, rgba(255, 255, 255, 0.79) 0%, rgba(255, 255, 255, 0.35) 100%)" backdropFilter="blur(2px)" borderRadius="15px">
+            <Text mb="60px" fontSize="24px" fontWeight={700} color="#0F0F0F">
               Login
             </Text>
             <JSONForm formState={w3s.user.loginForm} />
           </Box>
         </Flex>
-      </Center>
+      </Flex>
     </Box>
   );
 });
