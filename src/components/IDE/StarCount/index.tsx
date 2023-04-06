d7669861974a5e4584e9737e3e55cc24659ce436
osdodo <osdodo@qq.com>
2023-04-06 16:55:29 +0800

    update github repository URL

--- src/components/IDE/StarCount/index.tsx
+++ src/components/IDE/StarCount/index.tsx
@@ -6,14 +6,14 @@ import { TbMinusVertical } from 'react-icons/tb';
 const StarCount = () => {
   const [count, setCount] = React.useState(0);
   React.useEffect(() => {
-    fetch('https://api.github.com/repos/machinefi/w3bstream-studio')
+    fetch('https://api.github.com/repos/machinefi/w3bstream')
       .then((res) => res.json())
       .then((data) => {
         setCount(data.stargazers_count);
       });
   }, []);
   return (
-    <Link display="flex" mr="20px" alignItems="center" p="10px 20px" bg="#F3F3F3" borderRadius="60px" href="https://github.com/machinefi/w3bstream-studio" isExternal>
+    <Link display="flex" mr="20px" alignItems="center" p="10px 20px" bg="#F3F3F3" borderRadius="60px" href="https://github.com/machinefi/w3bstream" isExternal>
       <Icon as={AiOutlineStar} />
       <chakra.span ml="10px" fontSize="14px" fontWeight={700}>
         Star
