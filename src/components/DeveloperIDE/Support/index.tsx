c7d7a1916c5d518ff31d76e83fbe83fca202f3e0
Jane <m13221001019@163.com>
2023-04-17 10:04:22 +0800

    update support

--- src/components/DeveloperIDE/Support/index.tsx
+++ src/components/DeveloperIDE/Support/index.tsx
@@ -11,20 +11,22 @@ const Support = () => {
     <Center w="100%" h="calc(100vh - 100px)">
       <Grid templateRows="repeat(2, 1fr)" templateColumns="repeat(3, 1fr)" gap={6}>
         <GridItem w="412px" h="328px" bg="#fff">
-          <Flex
-            w="100%"
-            h="100%"
-            flexDir="column"
-            justifyContent="center"
-            alignItems="center"
-            cursor="pointer"
-            _hover={{
-              boxShadow: 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px'
-            }}
-          >
-            <Image src="/images/icons/documentation.svg" />
-            <Box mt="32px">Documentation</Box>
-          </Flex>
+          <a href="http://docs.w3bstream.com" target="_blank" rel="noopener noreferrer">
+            <Flex
+              w="100%"
+              h="100%"
+              flexDir="column"
+              justifyContent="center"
+              alignItems="center"
+              cursor="pointer"
+              _hover={{
+                boxShadow: 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px'
+              }}
+            >
+              <Image src="/images/icons/documentation.svg" />
+              <Box mt="32px">Documentation</Box>
+            </Flex>
+          </a>
         </GridItem>
         <GridItem w="412px" h="328px" bg="#fff">
           <Flex
@@ -43,20 +45,22 @@ const Support = () => {
           </Flex>
         </GridItem>
         <GridItem w="412px" h="328px" bg="#fff">
-          <Flex
-            w="100%"
-            h="100%"
-            flexDir="column"
-            justifyContent="center"
-            alignItems="center"
-            cursor="pointer"
-            _hover={{
-              boxShadow: 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px'
-            }}
-          >
-            <Image src="/images/icons/apply-for-grant.svg" />
-            <Box mt="32px">Apply for grant</Box>
-          </Flex>
+          <a href="http://developers.iotex.io/grants" target="_blank" rel="noopener noreferrer">
+            <Flex
+              w="100%"
+              h="100%"
+              flexDir="column"
+              justifyContent="center"
+              alignItems="center"
+              cursor="pointer"
+              _hover={{
+                boxShadow: 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px'
+              }}
+            >
+              <Image src="/images/icons/apply-for-grant.svg" />
+              <Box mt="32px">Apply for grant</Box>
+            </Flex>
+          </a>
         </GridItem>
         <GridItem w="412px" h="328px" bg="#fff">
           <Flex
@@ -76,21 +80,22 @@ const Support = () => {
           </Flex>
         </GridItem>
         <GridItem w="412px" h="328px" bg="#fff">
-          <Flex
-            w="100%"
-            h="100%"
-            flexDir="column"
-            justifyContent="center"
-            alignItems="center"
-            cursor="pointer"
-            _hover={{
-              boxShadow: 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px'
-            }}
-            onClick={onOpen}
-          >
-            <Image src="/images/icons/about.svg" />
-            <Box mt="32px">Tutorials</Box>
-          </Flex>
+          <a href="http://developers.iotex.io/academy" target="_blank" rel="noopener noreferrer">
+            <Flex
+              w="100%"
+              h="100%"
+              flexDir="column"
+              justifyContent="center"
+              alignItems="center"
+              cursor="pointer"
+              _hover={{
+                boxShadow: 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px'
+              }}
+            >
+              <Image boxSize={'70px'} color="#000" src="/images/tutorials.svg" />
+              <Box mt="32px">Tutorials</Box>
+            </Flex>
+          </a>
         </GridItem>
       </Grid>
       <Modal onClose={onClose} isOpen={isOpen} isCentered>
