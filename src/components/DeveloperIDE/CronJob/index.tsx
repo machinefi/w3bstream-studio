829f508d2a1c7d8c98ac1255b78976be71b252bf
osdodo <osdodo@qq.com>
2023-04-19 15:50:15 +0800

    add cron monitor (#106)

---
+++ src/components/DeveloperIDE/CronJob/index.tsx
@@ -0,0 +1,69 @@
+import { observer } from 'mobx-react-lite';
+import { useStore } from '@/store/index';
+import { Button } from '@chakra-ui/react';
+import { AddIcon } from '@chakra-ui/icons';
+import { defaultButtonStyle } from '@/lib/theme';
+import JSONTable from '@/components/JSONTable';
+import { hooks } from '@/lib/hooks';
+import { axios } from '@/lib/axios';
+import { eventBus } from '@/lib/event';
+import { useEffect } from 'react';
+
+const CronJobs = observer(() => {
+  const {
+    w3s: {
+      cronJob,
+      project: { curProject }
+    }
+  } = useStore();
+
+  useEffect(() => {
+    if (curProject) {
+      cronJob.fetchCronJobs(curProject.f_project_id);
+    }
+  }, [curProject]);
+
+  return <JSONTable jsonstate={cronJob} />;
+});
+
+export const CreateCronJobButton = observer(() => {
+  const {
+    w3s: {
+      cronJob,
+      project: { curProject }
+    }
+  } = useStore();
+  return (
+    <Button
+      h="32px"
+      leftIcon={<AddIcon />}
+      {...defaultButtonStyle}
+      onClick={async (e) => {
+        const formData = await hooks.getFormData({
+          title: 'Create Cron Job',
+          size: 'md',
+          formList: [
+            {
+              form: cronJob.form
+            }
+          ]
+        });
+        const projectId = curProject?.f_project_id;
+        if (projectId && formData.cronExpressions) {
+          const res = await axios.request({
+            method: 'post',
+            url: `/api/w3bapp/cronjob/${projectId}`,
+            data: formData
+          });
+          if (res.data) {
+            eventBus.emit('cronJob.create', projectId);
+          }
+        }
+      }}
+    >
+      Create
+    </Button>
+  );
+});
+
+export default CronJobs;
---
+++ src/store/lib/w3bstream/schema/cronJob.ts
@@ -0,0 +1,105 @@
+import { JSONSchemaFormState, JSONSchemaTableState, JSONValue } from '@/store/standard/JSONSchemaState';
+import { FromSchema } from 'json-schema-to-ts';
+import { CronJobsType } from '@/server/routers/w3bstream';
+import { trpc } from '@/lib/trpc';
+import { defaultOutlineButtonStyle } from '@/lib/theme';
+import { axios } from '@/lib/axios';
+import { eventBus } from '@/lib/event';
+import toast from 'react-hot-toast';
+
+export const schema = {
+  type: 'object',
+  properties: {
+    eventType: { type: 'string', title: 'Event Type' },
+    cronExpressions: { type: 'string', title: 'Cron Expressions' }
+  },
+  required: ['eventType', 'cronExpressions']
+} as const;
+
+type SchemaType = FromSchema<typeof schema>;
+
+export default class CronJobModule {
+  table = new JSONSchemaTableState<CronJobsType>({
+    columns: [
+      {
+        key: 'actions',
+        label: 'Actions',
+        actions: (item) => {
+          return [
+            {
+              props: {
+                size: 'xs',
+                ...defaultOutlineButtonStyle,
+                onClick() {
+                  globalThis.store.base.confirm.show({
+                    title: 'Warning',
+                    description: 'Are you sure you want to delete it?',
+                    async onOk() {
+                      try {
+                        await axios.request({
+                          method: 'delete',
+                          url: `/api/w3bapp/cronjob/${item.f_project_id}/${item.f_cron_job_id}`
+                        });
+                        eventBus.emit('cronJob.delete', item.f_project_id);
+                        toast.success('Deleted successfully');
+                      } catch (error) {
+                        toast.error('Delete failed');
+                      }
+                    }
+                  });
+                }
+              },
+              text: 'Delete'
+            }
+          ];
+        }
+      },
+      {
+        key: 'f_cron_expressions',
+        label: 'Cron Expressions'
+      },
+      {
+        key: 'f_event_type',
+        label: 'Event Type'
+      },
+      {
+        key: 'f_created_at',
+        label: 'Created At'
+      }
+    ],
+    rowKey: 'f_cron_job_id',
+    containerProps: { mt: '10px' }
+  });
+
+  form = new JSONSchemaFormState<SchemaType>({
+    //@ts-ignore
+    schema,
+    uiSchema: {
+      'ui:submitButtonOptions': {
+        norender: false,
+        submitText: 'Submit'
+      }
+    },
+    afterSubmit: async (e) => {
+      eventBus.emit('base.formModal.afterSubmit', e.formData);
+      this.form.reset();
+    },
+    value: new JSONValue<SchemaType>({
+      default: {
+        eventType: 'DEFAULT',
+        cronExpressions: '* * * * *'
+      }
+    })
+  });
+
+  async fetchCronJobs(projectId) {
+    const res = await trpc.api.cronJobs.query({
+      projectId
+    });
+    if (res) {
+      this.table.set({
+        dataSource: res
+      });
+    }
+  }
+}
