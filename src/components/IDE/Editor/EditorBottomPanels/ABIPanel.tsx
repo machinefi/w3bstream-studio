3ed31301222662f854ded83aea4a17d06fad054f
DLHTX <949729789@qq.com>
2023-05-05 18:21:27 +0800

    support lab abi

---
+++ src/components/IDE/Editor/EditorBottomPanels/ABIPanel.tsx
@@ -0,0 +1,351 @@
+import React, { useEffect } from 'react';
+import { observer, useLocalObservable } from 'mobx-react-lite';
+import { useStore } from '@/store/index';
+import { Box } from '@chakra-ui/layout';
+import {
+  Flex,
+  Text,
+  Icon,
+  InputGroup,
+  InputLeftAddon,
+  Input,
+  InputRightAddon,
+  Link,
+  Card,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+  Accordion,
+  VStack,
+  Button,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  FormLabel,
+  AccordionIcon,
+  FormControl,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Select,
+  Alert,
+  AlertIcon,
+  AlertDescription,
+  Checkbox
+} from '@chakra-ui/react';
+import { MinusIcon, LinkIcon, RepeatIcon, CopyIcon } from '@chakra-ui/icons';
+import { toJS } from 'mobx';
+import { helper, toast } from '@/lib/helper';
+import { ContractInstance, FunctionState } from '@/store/lib/ContractInstance';
+import { BooleanState } from '@/store/standard/base';
+import BigNumber from 'bignumber.js';
+import { ethers } from 'ethers';
+import { Copy } from '@/components/Common/Copy';
+
+export const ABIPanel = observer(() => {
+  const {
+    w3s,
+    w3s: {
+      projectManager: { curFilesListSchema },
+      lab
+    }
+  } = useStore();
+  const store = useLocalObservable<{ curContractInstance: ContractInstance }>(() => ({
+    curContractInstance: null
+  }));
+
+  useEffect(() => {
+    if (curFilesListSchema.curActiveFile.data.dataType == 'abi') {
+      const codeJSON = helper.json.safeParse(curFilesListSchema.curActiveFile.data.code);
+      let abi;
+      if (Array.isArray(codeJSON)) {
+        abi = codeJSON;
+      } else if (codeJSON.abi) {
+        abi = codeJSON.abi;
+      }
+      store.curContractInstance = new ContractInstance({
+        abi,
+        name: curFilesListSchema.curActiveFile.label,
+        address: codeJSON.address ?? ''
+      });
+    }
+  }, [curFilesListSchema.curActiveFile, curFilesListSchema.curActiveFile.data.code]);
+
+  return (
+    <>
+      <Flex color="white" fontFamily={'sans-serif'} height="calc(100%)" overflowY={'scroll'} w="100%">
+        <Box position="relative" w="100%">
+          <Card bg="none" borderRadius={0} w="100%">
+            <Box overflow="auto" h="100%" w="100%">
+              <InputGroup size={'sm'}>
+                <InputLeftAddon children="Name" />
+                <Input readOnly placeholder="please input the address" value={store.curContractInstance?.name} />
+              </InputGroup>
+              <InputGroup size={'sm'} my="4px">
+                <InputLeftAddon children="Address" />
+                <Input
+                  onChange={(e) => {
+                    store.curContractInstance.address = e.target.value;
+                    console.log(store.curContractInstance.address);
+                  }}
+                  placeholder="please input the address"
+                  value={store.curContractInstance?.address}
+                />
+                <InputRightAddon
+                  children={
+                    <Link href={``} target="__blank">
+                      <LinkIcon />
+                    </Link>
+                  }
+                />
+              </InputGroup>
+              <FunctionList contract={store.curContractInstance} />
+            </Box>
+          </Card>
+        </Box>
+      </Flex>
+    </>
+  );
+});
+
+export const FunctionList = observer(({ contract }: { contract: ContractInstance }) => {
+  const { god } = useStore();
+  const {
+    w3s: {
+      projectManager: { curFilesListSchema }
+    }
+  } = useStore();
+
+  return (
+    <Tabs size="sm" isLazy onChange={(i) => contract.setTabIndex(i)} index={contract?.tabIndex}>
+      <TabList>
+        <Tab>Read</Tab>
+        <Tab>Write</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>
+          <Accordion allowMultiple>
+            {contract?.readFunctions.map((functionItem) => (
+              <FunctiomItem functionItem={functionItem} contract={contract} key={functionItem.name} />
+            ))}
+          </Accordion>
+        </TabPanel>
+        <TabPanel>
+          <Accordion allowMultiple>
+            {contract?.writeFunction.map((functionItem) => (
+              <FunctiomItem functionItem={functionItem} contract={contract} key={functionItem.name} />
+            ))}
+          </Accordion>
+        </TabPanel>
+        <TabPanel>
+          <Box overflow="auto">
+            {contract?.execHistory.map((i) => (
+              <Box key={i.hash} fontSize="xs" wordBreak="break-all">
+                <VStack spacing="1" align="stretch" p={2} borderRadius="5" mt="2" border="1px">
+                  <Text fontSize="md" fontWeight="500">
+                    {i.method}
+                  </Text>
+                  <Box>
+                    {i.params?.map((v, i) => (
+                      <Text>
+                        {i}: {v}
+                      </Text>
+                    ))}
+                  </Box>
+                  <Link color="twitter.500" href={`${god.currentChain.explorerURL}/tx/${i.hash}`} target="__blank">
+                    {i.hash}
+                  </Link>
+                </VStack>
+              </Box>
+            ))}
+            {contract?.execHistory.length > 0 && (
+              <Button mt={8} size="xs" onClick={(e) => contract.clearExecHistory()}>
+                Clear History
+              </Button>
+            )}
+          </Box>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
+  );
+});
+
+export const FunctiomItem = observer(({ functionItem, contract }: { functionItem: FunctionState; contract: ContractInstance }) => {
+  const { god } = useStore();
+  const _contract = contract;
+  const store = useLocalObservable(() => ({
+    loading: new BooleanState(),
+    async onCall(func: FunctionState, address) {
+      console.log(contract);
+      if (!address) {
+        return toast.warning('Please set address');
+      }
+      func.setCallError('');
+      const options = {} as any;
+      if (func.amount?.value) {
+        options.value = func.amount.value;
+      }
+      if (func.gasLimit) {
+        options.gasLimit = func.gasLimit;
+      }
+
+      store.loading.setValue(true);
+      const params = func.inputs.map((i) => {
+        i = helper.json.safeParse(i.value);
+        if (typeof i == 'number') {
+          return new BigNumber(i).toFixed();
+        }
+        return i;
+      });
+      console.log(params);
+
+      const [err, res] = await helper.promise.runAsync(
+        god.currentNetwork.execContract({
+          address,
+          abi: contract.abi,
+          method: func.name,
+          params,
+          options
+        })
+      );
+
+      if (!err) {
+        if (res.hash) {
+          func.setCallResult(res.hash);
+          // @ts-ignore
+          // store.curContractInstance.addExecHistory({ method: functionItem.name, params, options, address: store.curContractInstance.address, hash: res.hash });
+          res.wait();
+        } else {
+          func.setCallResult(res.toString());
+        }
+      }
+      store.loading.setValue(false);
+      if (err) {
+        console.log(err);
+        functionItem.setCallError(err.message);
+      }
+    },
+    async copyByteCode(func: FunctionState) {
+      try {
+        const params = func.inputs.map((i) => {
+          i = helper.json.safeParse(i.value);
+          if (typeof i == 'number') {
+            return new BigNumber(i).toFixed();
+          }
+          return i;
+        });
+        const abi = contract.abi;
+        const Interface = new ethers.utils.Interface(abi);
+        const bytecode = Interface.encodeFunctionData(functionItem.name, params);
+        await navigator.clipboard.writeText(bytecode);
+        toast.success('Copy Success');
+      } catch (e) {
+        toast.error(e.message);
+      }
+    }
+  }));
+  return (
+    <AccordionItem key={functionItem.name}>
+      <AccordionButton color={functionItem.background} py={1.5} px={0}>
+        <Box flex="1" textAlign="left" fontSize="sm" fontWeight={500}>
+          {functionItem.name}（{functionItem.inputs?.length || 0}）
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+      <AccordionPanel py={2} px={2}>
+        {functionItem.inputs?.length ? (
+          <Box>
+            {functionItem.inputs.map((input) => (
+              <FormControl key={input.name}>
+                <FormLabel fontSize="xs" mb={0.5}>
+                  {input.name}
+                </FormLabel>
+                <Input size="xs" mb={1.5} fontSize="sm" value={input.value} onChange={(e) => input.setValue(e.target.value)} placeholder={input.type} />
+              </FormControl>
+            ))}
+          </Box>
+        ) : null}
+        <Flex mt="8px" alignItems="center">
+          <Button isDisabled={contract.address == ''} onClick={(e) => store.onCall(functionItem, contract.address)} size="sm" isLoading={store.loading.value}>
+            Submit
+          </Button>
+          <Button ml={2} onClick={(e) => store.copyByteCode(functionItem)} size="sm">
+            Copy bytecode
+          </Button>
+
+          {['payable', 'nonpayable'].includes(functionItem.stateMutability) && (
+            <Checkbox size="sm" isChecked={functionItem.showCustom} ml={4} onChange={(e) => functionItem.setShowCustom(e.target.checked)}>
+              Custom
+            </Checkbox>
+          )}
+        </Flex>
+        {functionItem.showCustom && (
+          <FormControl mt="8px">
+            <FormLabel fontSize="xs" mb={0.5}>
+              GAS LIMIT
+            </FormLabel>
+            <NumberInput defaultValue={3000000} onChange={(e) => functionItem.setGasLimit(e)} size="xs" min={0} mb={1.5}>
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+            {functionItem.amount && (
+              <Box>
+                <FormLabel fontSize="xs" mb={0.5}>
+                  VALUE
+                </FormLabel>
+                <InputGroup size="xs">
+                  <Input defaultValue={0} mb={1.5} value={functionItem.amount.input} onChange={(e) => functionItem.amount.setInput(e.target.value)} placeholder={'please enter amount'} />
+                  <InputRightAddon>
+                    <Select size="xs" value={functionItem.amount.unit} onChange={(e) => functionItem.amount.setUnit(e.target.value)} border="none">
+                      {Object.keys(functionItem.amount.unitConfig).map((key) => (
+                        <option key={key} value={key}>
+                          {key}
+                        </option>
+                      ))}
+                    </Select>
+                  </InputRightAddon>
+                </InputGroup>
+              </Box>
+            )}
+          </FormControl>
+        )}
+        {functionItem.callError && (
+          <Alert status="error" mt={2}>
+            <AlertIcon />
+            <AlertDescription fontSize="sm">{functionItem.callError}</AlertDescription>
+          </Alert>
+        )}
+      </AccordionPanel>
+      {functionItem.callResult && <CallResult text={functionItem.callResult} />}
+    </AccordionItem>
+  );
+});
+
+export const CallResult = observer(({ text }: { text: string }) => {
+  const { god } = useStore();
+  let detail = <Text>{text}</Text>;
+  if (text.startsWith('0x')) {
+    const route = text.length == 42 ? 'address' : 'tx';
+    detail = (
+      <>
+        <Link color="twitter.500" href={`${god.currentChain.explorerURL}/${route}/${text}`} target="__blank">
+          {text}
+        </Link>
+        <Copy value={text}></Copy>
+      </>
+    );
+  }
+
+  return (
+    <Box fontSize="xs" ml={2} pb={2}>
+      {detail}
+    </Box>
+  );
+});
---
+++ src/components/IDE/Editor/EditorBottomPanels/DBpanel.tsx
@@ -0,0 +1,129 @@
+import JSONTable from '@/components/JSONTable';
+import dayjs from '@/lib/dayjs';
+import { eventBus } from '@/lib/event';
+import { TableJSONSchema } from '@/server/wasmvm/sqldb';
+import { useStore } from '@/store/index';
+import { JSONSchemaTableState } from '@/store/standard/JSONSchemaState';
+import { Box, Flex, Input, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
+import { toJS } from 'mobx';
+import { observer, useLocalObservable } from 'mobx-react-lite';
+import { useEffect, useRef } from 'react';
+import toast from 'react-hot-toast';
+import { VscClearAll, VscDebugStart } from 'react-icons/vsc';
+
+export const DBpanel = observer(() => {
+  const {
+    god: { sqlDB },
+    w3s,
+    w3s: {
+      projectManager: { curFilesListSchema },
+      lab
+    }
+  } = useStore();
+  const store = useLocalObservable(() => ({
+    tables: [],
+    sql: ``,
+    initDBTable: () => {
+      store.tables = [];
+      try {
+        const sqlJSON: TableJSONSchema[] | TableJSONSchema = JSON.parse(curFilesListSchema.curActiveFile?.data?.code);
+        const _sqlJSON = Array.isArray(sqlJSON) ? sqlJSON : [sqlJSON];
+        const tables: { tableName: string; columnName: string[]; table: JSONSchemaTableState }[] = [];
+        _sqlJSON.forEach((i) => {
+          const { tableName, columnName } = sqlDB.getTableInfoByJSONSchema(i);
+          console.log(tableName, columnName);
+          // sqlDB.test();
+          const res = sqlDB.db.exec(`SELECT * FROM ${tableName}`);
+          console.log(res);
+          const dataSource: { [key: string]: any }[] = [];
+          if (res.length > 0) {
+            res[0].values.forEach((i) => {
+              const obj: { [key: string]: any } = {};
+              i.forEach((j, index) => {
+                obj[columnName[index]] = j;
+              });
+              dataSource.push(obj);
+            });
+          }
+
+          tables.push({
+            tableName,
+            columnName,
+            table: new JSONSchemaTableState<any>({
+              dataSource,
+              columns: columnName.map((i) => {
+                return {
+                  key: i,
+                  label: i
+                };
+              })
+              // containerProps: { mt: 4, h: 'calc(100vh - 200px)', overflowY: 'auto' }
+            })
+          });
+          store.tables = tables;
+          console.log(toJS(tables));
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }));
+  useEffect(() => {
+    eventBus.on('sql.change', store.initDBTable);
+    return () => {
+      eventBus.off('sql.change', store.initDBTable);
+    };
+  }, []);
+
+  useEffect(() => {
+    store.initDBTable();
+  }, [curFilesListSchema.curActiveFile]);
+
+  return (
+    <Box mt={2}>
+      {store.tables.length > 0 && (
+        <>
+          <Flex align="center">
+            <Input placeholder="write sql here" w="96%" value={store.sql} onChange={(e) => (store.sql = e.target.value)}></Input>
+            <VscDebugStart
+              style={{ marginLeft: 'auto', marginRight: '10px' }}
+              fontSize={'19px'}
+              onClick={(e) => {
+                try {
+                  sqlDB.exec(store.sql);
+                } catch (e) {
+                  console.log(e);
+                  toast.error(e.message);
+                }
+                store.initDBTable();
+              }}
+            ></VscDebugStart>
+          </Flex>
+          <Tabs>
+            <TabList>
+              {store?.tables?.map((i) => {
+                return <Tab>{i.tableName}</Tab>;
+              })}
+            </TabList>
+
+            <Box mt={2} id="terminal" fontFamily="monospace" w="100%" h="calc(100vh - 630px)" whiteSpace="pre-line" overflowY="auto" position="relative">
+              <TabPanels>
+                {store?.tables?.map((i) => {
+                  return (
+                    <TabPanel p={0}>
+                      <JSONTable
+                        jsonstate={{
+                          table: i.table
+                        }}
+                      />
+                    </TabPanel>
+                  );
+                })}
+              </TabPanels>
+            </Box>
+          </Tabs>
+        </>
+      )}
+    </Box>
+  );
+});
