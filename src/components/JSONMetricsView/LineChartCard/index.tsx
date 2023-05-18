cacf93525f402385930bf188e59ac04835d1912e
osdodo <osdodo@qq.com>
2023-05-18 20:31:56 +0800

    update metrics (#165)
    * update metrics
    
    * fix metrics

--- src/components/JSONMetricsView/LineChartCard/index.tsx
+++ src/components/JSONMetricsView/LineChartCard/index.tsx
@@ -1,56 +1,107 @@
-import { Box, Card, CardBody, CardHeader, Heading, Select, Stack, Flex } from '@chakra-ui/react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { Box, Card, CardBody, CardHeader, Heading, Stack, Flex } from '@chakra-ui/react';
+import { ResponsiveLine } from '@nivo/line';
 import { SiSimpleanalytics } from 'react-icons/si';
 
 export interface LineChartCard {
   title: string;
-  tips?: string;
-  data: { name: string; value: number; extraData?: { [key: string]: any } }[];
+  description?: string;
+  data: {
+    id: string;
+    data: {
+      x: string;
+      y: number;
+    }[];
+  }[],
   config?: {
     containerWidth?: string | number;
     containerHeight?: string | number;
-    lineColor?: string;
-    lineWidth?: number;
-    lineType?: 'monotone' | 'natural' | 'basis' | 'linear';
-    dotColor?: string;
   };
 }
 
-export const LineChartCard = ({ title, tips, data, config = {} }: LineChartCard) => {
-  const { containerWidth = '100%', containerHeight = 200, lineColor = '#946FFF', lineWidth = 2, lineType = 'monotone', dotColor = '#946FFF' } = config;
+export const LineChartCard = ({ title, description, data, config = {} }: LineChartCard) => {
+  const { containerWidth = '100%', containerHeight = 400 } = config;
 
   return (
     <Card>
       <CardHeader>
         <Flex alignItems={'center'}>
           <Heading size="md">{title}</Heading>
-        
         </Flex>
-      </CardHeader>
-      <CardBody pt="0">
-        {tips && (
-          <Box fontWeight={700} mb="10px">
-            {tips}
+        {description && (
+          <Box>
+            {description}
           </Box>
         )}
-        {data.length > 0 ? (
-          <ResponsiveContainer width={containerWidth} height={containerHeight}>
-            <LineChart data={data}>
-              <XAxis dataKey="name" />
-              <YAxis />
-              <CartesianGrid />
-              <Tooltip
-                labelFormatter={(index) => index}
-                // formatter={(value, name) => {
-                //   const cur = data.find((item) => item[0] === name);
-                //   return value;
-                // }}
-              />
-              <Line type={lineType} dataKey="value" stroke={lineColor} dot={{ fill: dotColor }} strokeWidth={lineWidth} />
-            </LineChart>
-          </ResponsiveContainer>
+      </CardHeader>
+      <CardBody pt="0" >
+        {data[0]?.data.length > 0 ? (
+          <Box w={containerWidth} h={containerHeight} >
+            <ResponsiveLine
+              data={data}
+              margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+              xScale={{ type: 'point' }}
+              yScale={{
+                type: 'linear',
+                min: 'auto',
+                max: 'auto',
+                stacked: true,
+                reverse: false
+              }}
+              yFormat=" >-.2f"
+              axisTop={null}
+              axisRight={null}
+              axisBottom={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: 'transportation',
+                legendOffset: 36,
+                legendPosition: 'middle'
+              }}
+              axisLeft={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: 'count',
+                legendOffset: -40,
+                legendPosition: 'middle'
+              }}
+              pointSize={10}
+              pointColor={{ theme: 'background' }}
+              pointBorderWidth={2}
+              pointBorderColor={{ from: 'serieColor' }}
+              pointLabelYOffset={-12}
+              useMesh={true}
+              legends={[
+                {
+                  anchor: 'bottom-right',
+                  direction: 'column',
+                  justify: false,
+                  translateX: 100,
+                  translateY: 0,
+                  itemsSpacing: 0,
+                  itemDirection: 'left-to-right',
+                  itemWidth: 80,
+                  itemHeight: 20,
+                  itemOpacity: 0.75,
+                  symbolSize: 12,
+                  symbolShape: 'circle',
+                  symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                  effects: [
+                    {
+                      on: 'hover',
+                      style: {
+                        itemBackground: 'rgba(0, 0, 0, .03)',
+                        itemOpacity: 1
+                      }
+                    }
+                  ]
+                }
+              ]}
+            />
+          </Box>
         ) : (
-          <Stack align="center">
+          <Stack minH="100px" align="center" justify="center">
             <SiSimpleanalytics />
             <Box>No data to show</Box>
           </Stack>
--- src/pages/api/metrics/[...slug].ts
+++ src/pages/api/metrics/[...slug].ts
@@ -2,18 +2,16 @@ import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { method, query, body, headers } = req;
+  const { method, query } = req;
   const { slug, ...params } = query;
   const path = Array.isArray(slug) ? slug.join('/') : slug;
   const url = `${process.env.NEXT_PUBLIC_METRICS_URL}/api/v1/${path}`;
-  console.log(url, method, params, body, headers);
   try {
     const axiosResponse = await axios.request({
       url,
-      headers,
       params,
-      method: method as 'GET' | 'POST' | 'PUT' | 'DELETE',
-      data: body
+      // @ts-ignore
+      method
     });
     res.status(axiosResponse.status).json(axiosResponse.data);
   } catch (error) {
