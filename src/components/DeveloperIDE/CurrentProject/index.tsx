edb82ce4033ba377fb40c4da9502699b1f487114
DLHTX <35157091+DLHTX@users.noreply.github.com>
2023-05-19 12:48:14 +0800

    feat(lab): add refresh button & connect button (#164)
    * feat(lab): add refresh button & connect button
    
    * update vscode remote file type
    
    * fix build error

--- src/components/DeveloperIDE/CurrentProject/index.tsx
+++ src/components/DeveloperIDE/CurrentProject/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Flex, Box } from '@chakra-ui/react';
+import { Flex, Box, Tooltip, Button } from '@chakra-ui/react';
 import { observer } from 'mobx-react-lite';
 import { useStore } from '@/store/index';
 import ToolBar from '../ToolBar';
@@ -10,26 +10,59 @@ import DBTable from '../DeveloperDBTable';
 import EventLogs from '@/components/IDE/EventLogs';
 import Settings from '../Settings';
 import { ChevronLeftIcon } from '@chakra-ui/icons';
+import { MdRefresh } from 'react-icons/md';
+import { UnstyledButton } from '@mantine/core';
 
 const CurrentProject = observer(() => {
   const {
+    w3s,
     w3s: { showContent, project }
   } = useStore();
 
   return (
-    <Flex w="100%">
-      <ToolBar borderRadius="8px" overflowY="auto" />
-      <Box ml="220px" w="100%" p="20px" bg="#fff" boxSizing='content-box' borderRadius="8px">
-        {showContent === 'METRICS' && <Metrics />}
-        {showContent === 'CURRENT_PUBLISHERS' && <Publishers />}
-        {(showContent === 'CONTRACT_LOGS' || showContent === 'CHAIN_TX' || showContent === 'CHAIN_HEIGHT') && <Triggers />}
-        {showContent === 'DB_TABLE' && <DBTable />}
-        {showContent === 'CURRENT_EVENT_LOGS' && <EventLogs />}
-        {showContent === 'SETTINGS' && <Settings />}
-      </Box>
-    </Flex>
+    <Box>
+      <Flex
+        mb="15px"
+        alignItems="center"
+        color="#0F0F0F"
+        fontSize="14px"
+        cursor="pointer"
+        bg="#fff"
+        p="10px 15px"
+        w="86px"
+        borderRadius="4px"
+        onClick={() => {
+          project.allProjects.onSelect(-1);
+          project.resetSelectedNames();
+        }}
+      >
+        <ChevronLeftIcon />
+        <Box ml="10px">Back</Box>
+      </Flex>
+      <Flex w="100%" h="calc(100vh - 180px)" overflow={'hidden'} position="relative">
+        <ToolBar borderRadius="8px" overflowY="auto" />
+        <Box ml="20px" w="100%" h="100%" p="20px" pb="50px" bg="#fff" boxSizing="content-box" borderRadius="8px" overflow={'auto'}>
+          <Flex mb={4}>
+            <Button
+              ml="auto"
+              size="sm"
+              onClick={(e) => {
+                w3s.init();
+              }}
+            >
+              <MdRefresh />
+            </Button>
+          </Flex>
+          {showContent === 'METRICS' && <Metrics />}
+          {showContent === 'CURRENT_PUBLISHERS' && <Publishers />}
+          {(showContent === 'CONTRACT_LOGS' || showContent === 'CHAIN_TX' || showContent === 'CHAIN_HEIGHT') && <Triggers />}
+          {showContent === 'DB_TABLE' && <DBTable />}
+          {showContent === 'CURRENT_EVENT_LOGS' && <EventLogs />}
+          {showContent === 'SETTINGS' && <Settings />}
+        </Box>
+      </Flex>
+    </Box>
   );
 });
-
 
 export default CurrentProject;
