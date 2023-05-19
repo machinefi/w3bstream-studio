f05efecc947066eb2cec10e6a52fbe37a715525c
osdodo <osdodo@qq.com>
2023-05-19 16:45:36 +0800

    show account id and project id

--- src/components/DeveloperIDE/Header/index.tsx
+++ src/components/DeveloperIDE/Header/index.tsx
@@ -89,7 +89,9 @@ const Header = observer(() => {
           <Image mr="20px" w="100px" src="https://img.shields.io/github/stars/machinefi/w3bstream-studio.svg?style=social&label=Star&maxAge=2592000" />
         </ChakraLink> */}
         <StarCount />
-        <Profile />
+        <Flex mr="10px">
+          <Profile />
+        </Flex>
       </Flex>
     </Flex>
   );
