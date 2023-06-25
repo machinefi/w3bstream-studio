a7c0d13889dbf1d39acf530dd11f72407f8b628e
DLHTX <35157091+DLHTX@users.noreply.github.com>
2023-06-25 12:57:43 +0800

    feat(event): add start/pause all button (#235)

--- src/components/IDE/Monitor/ContractLogs/index.tsx
+++ src/components/IDE/Monitor/ContractLogs/index.tsx
@@ -19,51 +19,87 @@ const ContractLogs = observer(() => {
     });
   }, [w3s.contractLogs.curProjectContractLogs]);
 
-  return (
-    <JSONTable jsonstate={w3s.contractLogs} />
-  );
+  return <JSONTable jsonstate={w3s.contractLogs} />;
 });
 
 export const CreateContractLogButton = observer(() => {
   const { w3s } = useStore();
+  const projectName = w3s.project.curProject?.name;
+  const ids = w3s.project.curProject.contractLogs.map((item) => String(item.f_contractlog_id));
   return (
-    <Button
-      h="32px"
-      size="sm"
-      leftIcon={<AddIcon />}
-      {...defaultButtonStyle}
-      onClick={async (e) => {
-        w3s.contractLogs.form.value.set({
-          projectName: w3s.project.curProject?.name
-        });
-        w3s.contractLogs.form.uiSchema.projectName = {
-          'ui:widget': 'hidden'
-        };
-        const formData = await hooks.getFormData({
-          title: 'Add Smart Contract event monitor',
-          size: 'xl',
-          formList: [
-            {
-              form: w3s.contractLogs.form
-            }
-          ]
-        });
-        if (formData.projectName) {
-          formData.chainID = Number(formData.chainID);
-          const res = await axios.request({
-            method: 'post',
-            url: `/api/w3bapp/monitor/x/${formData.projectName}/contract_log`,
-            data: formData
+    <Flex>
+      <Button
+        mr={2}
+        h="32px"
+        size="sm"
+        {...defaultButtonStyle}
+        onClick={async () => {
+          await axios.request({
+            method: 'put',
+            url: `/api/w3bapp/monitor/x/${projectName}/contract_log/START`,
+            data: { ids }
           });
-          if (res.data) {
-            toast.success('Smart Contract event monitor successfully created.');
-            eventBus.emit('contractlog.create');
+          eventBus.emit('contractlog.delete');
+          toast.success('start successfully');
+        }}
+      >
+        Start all
+      </Button>
+      <Button
+        mr={2}
+        h="32px"
+        size="sm"
+        {...defaultButtonStyle}
+        onClick={async () => {
+          await axios.request({
+            method: 'put',
+            url: `/api/w3bapp/monitor/x/${projectName}/contract_log/PAUSE`,
+            data: { ids }
+          });
+          eventBus.emit('contractlog.delete');
+          toast.success('pause successfully');
+        }}
+      >
+        Pause all
+      </Button>
+      <Button
+        h="32px"
+        size="sm"
+        leftIcon={<AddIcon />}
+        {...defaultButtonStyle}
+        onClick={async (e) => {
+          w3s.contractLogs.form.value.set({
+            projectName: w3s.project.curProject?.name
+          });
+          w3s.contractLogs.form.uiSchema.projectName = {
+            'ui:widget': 'hidden'
+          };
+          const formData = await hooks.getFormData({
+            title: 'Add Smart Contract event monitor',
+            size: 'xl',
+            formList: [
+              {
+                form: w3s.contractLogs.form
+              }
+            ]
+          });
+          if (formData.projectName) {
+            formData.chainID = Number(formData.chainID);
+            const res = await axios.request({
+              method: 'post',
+              url: `/api/w3bapp/monitor/x/${formData.projectName}/contract_log`,
+              data: formData
+            });
+            if (res.data) {
+              toast.success('Smart Contract event monitor successfully created.');
+              eventBus.emit('contractlog.create');
+            }
           }
-        }
-      }}
-    >
-      Create
-    </Button>
+        }}
+      >
+        Create
+      </Button>
+    </Flex>
   );
 });
 
